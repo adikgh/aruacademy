94f5d4447b7728dd92b21a51f379c2ab3e5ba82d
adikgh <adiikgh@gmail.com>
2024-05-22 17:45:48 +0500

    2.22991

--- config/var.php
+++ config/var.php
@@ -6,7 +6,7 @@
    if (isset($_SESSION['lang'])) $lang = $_SESSION['lang'];
 
    // 
-   $ver = 2.2299;
+   $ver = 2.22991;
 
    // date - time
    $date = date("Y-m-d", time());
---
+++ op-onai/takyryp.php
@@ -0,0 +1,54 @@
+<? include "../config/core.php";
+
+
+
+   // 
+    $site_name = 'op-onai';
+    $menu_name = 'op-onai';
+    $site_set['header'] = false;
+    $site_set['footer'] = false;
+    $site_set['cl_wh'] = false;
+    $css = ['project/ln', 'project/op-onai'];
+    $js = ['project/ln'];
+
+
+
+?>
+<? include "../block/header.php"; ?>
+
+   <!--  -->
+   <div class="blo8" id="price">
+      <div class="bl_c">
+         <div class="blo8_c">
+            <div class="blo8_ci blo8_c78 blo8_c_mrf">
+               <!-- <h1 class="blo8_cin">ЖАУАПКЕРШІЛІКТІ БӨЛУ КЕСТЕСІ</h1> -->
+
+                <style>
+                    .body{
+                        background: var(--cs1);
+                    }
+                    .blo8{
+                        background: none;
+                    }
+                    .blo8_knn {
+                        padding-bottom: 135%;
+                    }
+                </style>
+
+               <div class="blo8_knn lazy_img" data-src="/assets/img/bag/SS ы_page-0001.jpg"></div>
+
+               <div class="blo8_cip ">
+                  <div class="blo8_cip_btn">
+                    <a class="btn btn_back" href="/assets/uploads/lesson/ЖАУАПКЕРШІЛІТІ БӨЛУ КЕСТЕСІ.pdf" download>
+                        <i class="far fa-cloud-download"></i>
+                        <span>Жүктеп алу</span>
+                    </a>
+                  </div>
+               </div>
+            </div>
+
+         </div>
+      </div>
+   </div>
+
+<? include "../block/footer.php"; ?>
